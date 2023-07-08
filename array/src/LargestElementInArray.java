155772ca7b9bbf0741a2976c115a8838cce1b1ed
jhanvi chaudhary <jhanvichaudhary32@gmail.com>
2023-07-08 22:49:23 +0530

    Largest Element in Array

--- array/src/LargestElementInArray.java
+++ array/src/LargestElementInArray.java
@@ -1,9 +1,11 @@
 
 //https://www.codingninjas.com/studio/problems/largest-element-in-the-array-largest-element-in-the-array_5026279?leftPanelTab=0
+
 public class LargestElementInArray {
     static int largestElementSolution1(int[] arr, int n) {
 
         int maxNumber=arr[0];
+
         for(int i=1;i<arr.length;i++){
             if(maxNumber<arr[i]){
                 maxNumber=arr[i];
